545d79c632a283a29112a786282c39e3ddb00ee5
Saurabh Charde <saurabhchardereal@gmail.com>
2020-01-06 22:38:48 +0530

    onc: init: Set dalvik-heap configurations based on RAM variant
    * Since we have 3 models with different amount of RAM
    
    * Values taken from frameworks/native
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- init/init_onclite.cpp
+++ init/init_onclite.cpp
@@ -81,6 +81,29 @@ void check_device()
    }
 }
 
+void set_dalvik_properties() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        // Set for 4GB RAM
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.6");
+        property_set("dalvik.vm.heapmaxfree", "16m");
+        property_set("dalvik.vm.heapminfree", "8m");
+    } else {
+        // Set for 2/3GB RAM
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("dalvik.vm.heapminfree", "512k");
+    }
+}
+
 void vendor_load_properties()
 {
     check_device();
